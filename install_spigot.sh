4c2a5e75ea822dbc6fb621ee997192101fde2e62
陳文傑 <jacch.tw@gmail.com>
2018-08-25 22:06:16 +0800

    Update and rename install_spigot.sh to install_spigot_1.13.sh

--- install_spigot.sh
+++
@@ -1,7 +0,0 @@
-
-
-#install openjdk9
-sudo apt-get install openjdk-9-jdk-headless
-
-#sudo apt-get install 
-java -jar BuildTools.jar --rev 1.13
---
+++ install_spigot_1.13.sh
@@ -0,0 +1,24 @@
+mkdir -p /home/pi/MCSERVER
+
+#Change Dir
+cd /home/pi/MCSERVER 
+
+#
+echo "cd /home/pi/MCSERVER" >run.sh
+echo "java -Xms920M -Xmx968M -jar spigot-1.13.jar " >> run.sh 
+
+#Download Spigot 
+wget https://hub.spigotmc.org/jenkins/job/BuildTools/75/artifact/target/BuildTools.jar
+
+#
+java -jar BuildTools.jar --rev 1.13
+
+#Run Minecraft for init 
+java -Xms920M -Xmx968M -jar spigot-1.13.jar
+
+echo "eula=true" >eula.txt
+
+#Start Spigot 
+java -Xms920M -Xmx968M -jar spigot-1.13.jar
+
+
